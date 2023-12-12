aa54a8b1ee7f861094b87fcc1efdf30c53bcfd40
wojcup <wojcup@gmail.com>
2023-12-12 11:35:30 +0000

    Seeders
    Adding static and dynamic seeders for User table.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // a fixed user:
+        User::factory()->create([
+            'name' => 'wojcup@email.com',
+            'email' => 'fixed@email.com',
+        ]);
+
+        // a fixed user:
+        User::factory()->create([
+            'name' => 'Instructor A',
+            'email' => 'wojcup+instructor1@email.com',
+            'role' => 'instructor',
+        ]);
+
+        // a fixed user:
+        User::factory()->create([
+            'name' => 'Instructor B',
+            'email' => 'wojcup+instructor2@email.com',
+            'role' => 'instructor',
+        ]);
+
+        // a fixed user:
+        User::factory()->create([
+            'name' => 'Admin A',
+            'email' => 'wojcup+admin1@email.com',
+            'role' => 'admin',
+        ]);
+
+
+        User::factory()->count(10)->create();
+        
+        User::factory()->count(10)->create([
+            'role' => 'instructor'
+        ]);
+
+
+        // DB::table('users')->insert(['name'=>'User Name', 'email' => 'user@email.com', 'role' => 'member']);
+        
+    }
+}
