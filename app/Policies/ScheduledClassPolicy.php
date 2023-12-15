8388d5789f5b402c02d6bc8660bb0b41c81b587d
wojcup <wojcup@gmail.com>
2023-12-15 11:42:26 +0000

    Tests
    Adding custom Instructor Tests

--- app/Policies/ScheduledClassPolicy.php
+++ app/Policies/ScheduledClassPolicy.php
@@ -11,6 +11,7 @@ class ScheduledClassPolicy
      * Create a new policy instance.
      */
     public function delete(User $user, ScheduledClass $scheduledClass){
-        return $user->id === $scheduledClass->instructor_id;
+        return  $user->id === $scheduledClass->instructor_id 
+                && $scheduledClass->date_time > now()->addHours(2);
     }
 }
